94acf5e203e515bda825b766ca52d7ece232546e
VDgit03 <dangvietdung2903@gmail.com>
2026-09-18 11:08:43 +0700

    main

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
     console.log("Server chạy tại http://localhost:3000");
