9eb2e15f7c5bf42d03af255f7b8540119fd8cdd8
VDgit03 <dangvietdung2903@gmail.com>
2026-09-25 22:24:34 +0700

    changepw

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,18 +2,19 @@ import express from "express";
 import { register } from "../controllers/auth/registerController.js";
 import { login } from "../controllers/auth/loginController.js";
 import { googleLogin } from "../controllers/auth/googleController.js";
-// // import { getUser } from "../controllers/custom/userController.js";
-// import { changePassword } from "../controllers/custom/changepwController.js";
+import { changePassword } from "../controllers/auth/changepwController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 // đăng kí, đăng nhập
 router.post("/register", register);
+
 router.post("/login", login);
+
 router.post("/google", googleLogin);
 
-// // lấy tên
-// router.get("/user/:id", getUser);
+// đổi mật khẩu
+router.put("/changepw", authMiddleware,changePassword);
 
-// // đổi mk
-// router.put("/changepw", changePassword);
 export default router;
